476b85ce4bf9a9da215411c87f421bb2478e0219
Tommy <ytommy0111@aol.com>
2023-01-09 10:16:06 -0500

    bruh moment

--- api/main.js
+++ api/main.js
@@ -1,36 +1,17 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>big guys</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      bruh
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
-
-const URL = "https://www.dnd5eapi.co/api/";
+const URL = "https://www.dnd5eapi.co/api/classes";
+// const URL = "https://api.quotable.io/random";
 
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
+    document.getElementById("api-response").textContent = data.results;
   } catch (error) {
     console.log(error);
   }
 }
 
 getData(URL);
+console.log(URL);
