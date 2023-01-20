fd7e3854f2f58a9b087f26666b796415c9165340
Tommy <ytommy0111@aol.com>
2023-01-20 10:16:38 -0500

    oijoijgdssapojkpogsdjoicxzojfsdu

--- api/main.js
+++ api/main.js
@@ -8,36 +8,10 @@ const DOMselectors = {
 
 let spellname = "fireball";
 
-//const URL = "https://www.dnd5eapi.co/api/classes";
-
-// const specificURL = `https://www.dnd5eapi.co/api/spells/${spellname}`;
 const specificURL = "https://www.dnd5eapi.co/api/spells/fireball";
 console.log(specificURL);
 
 const URL2 = "https://www.dnd5eapi.co/api/spells";
-
-//const URL3 = "https://www.dnd5eapi.co/api/alignments";
-
-// const URL = "https://api.quotable.io/random";
-
-/* async function getData(URL) {
-  try {
-    const response = await fetch(URL);
-    const data = await response.json();
-    data.results.forEach((classes) => {
-      console.log(classes.name);
-      data.results.forEach((classes) => {
-        document.body.insertAdjacentHTML(
-          "beforeend",
-          `<h2>Class: ${classes.name}</h2>`
-        );
-      });
-      idk;
-    });
-  } catch (error) {
-    console.log(error);
-  }
-} */
 
 async function getData2(URL2) {
   try {
@@ -64,6 +38,50 @@ async function descData1(specificURL) {
     const data = await response.json();
     data.desc.forEach((bruh) => {
       console.log(bruh);
+      DOMselectors.descbox.insertAdjacentHTML(
+        "beforeend",
+        `<h2>Name: ${data.name}</h2>`
+      );
+      DOMselectors.descbox.insertAdjacentHTML(
+        "beforeend",
+        `<h2>Level: ${data.level}</h2>`
+      );
+      DOMselectors.descbox.insertAdjacentHTML(
+        "beforeend",
+        `<h2>Cast Time: ${data.casting_time}</h2>`
+      );
+      DOMselectors.descbox.insertAdjacentHTML(
+        "beforeend",
+        `<h2>Duration: ${data.duration}</h2>`
+      );
+      DOMselectors.descbox.insertAdjacentHTML(
+        "beforeend",
+        `<h2>Range: ${data.range}</h2>`
+      );
+      DOMselectors.descbox.insertAdjacentHTML(
+        "beforeend",
+        `<h2>Components: ${data.components}</h2>`
+      );
+      DOMselectors.descbox.insertAdjacentHTML(
+        "beforeend",
+        `<h2>Ritual?: ${data.ritual}</h2>`
+      );
+      DOMselectors.descbox.insertAdjacentHTML(
+        "beforeend",
+        `<h2>DC Type: ${data.dc.dc_type.name}</h2>`
+      );
+      DOMselectors.descbox.insertAdjacentHTML(
+        "beforeend",
+        `<h2>Materials: ${data.material}</h2>`
+      );
+      DOMselectors.descbox.insertAdjacentHTML(
+        "beforeend",
+        `<h2>Damage Levels: ${data.damage.damage_at_slot_level}</h2>`
+      );
+      DOMselectors.descbox.insertAdjacentHTML(
+        "beforeend",
+        `<h2>Higher Level: ${data.higher_level}</h2>`
+      );
       data.desc.forEach((text) => {
         DOMselectors.descbox.insertAdjacentHTML(
           "beforeend",
@@ -90,25 +108,6 @@ async function descData1(specificURL) {
 }
 
 descData1(specificURL);
-
-/* async function getData3(URL3) {
-  try {
-    const response = await fetch(URL3);
-    const data = await response.json();
-    data.results.forEach((alignment) => {
-      console.log(alignment.name);
-      data.results.forEach((alignment) => {
-        document.body.insertAdjacentHTML(
-          "beforeend",
-          `<h4>Alignment: ${alignment.name}</h4>`
-        );
-      });
-      idk;
-    });
-  } catch (error) {
-    console.log(error);
-  }
-} */
 
 function makedesc() {
   const input = DOMselectors.input.value;
